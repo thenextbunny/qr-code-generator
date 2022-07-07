dd39a3d8991cc59d8a2a5200fdad201451bfc0e5
Vitor Coelho <vitorcoelhosilvat@gmail.com>
2022-07-06 21:16:12 -0300

    add

---
+++ assets/javascript/script.js
@@ -0,0 +1,18 @@
+const generateQR = document.querySelector('#qr-button');
+const inputQR = document.querySelector('#qr-input');
+
+inputQR.addEventListener('keyup', () => {
+    if (inputQR.value.length > 0) {
+        generateQR.disabled = false;
+        generateQR.style.background = '#159A9C';
+        generateQR.style.color = '#FFFFFF';
+    } else {
+        generateQR.disabled = true;
+        generateQR.style.background = '#B4BEC9';
+    }
+})
+
+generateQR.addEventListener('click', () => {
+    document.querySelector('#qr-image').src = `https://api.qrserver.com/v1/create-qr-code/?size=240x240&data=${inputQR.value}`;
+    document.querySelector('#qr-text').innerHTML = inputQR.value;
+})
